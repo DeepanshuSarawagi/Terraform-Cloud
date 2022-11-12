46d473d4053c08629eaaf60aae8a7b96713a7308
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2022-11-12 16:26:15 +0530

    Terraform cloud initial commit and setup

---
+++ Terraform-Cloud-Demo/ec2-create.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "ec2-east" {
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  availability_zone      = var.az
+  key_name               = var.key_name
+  user_data              = file("${path.module}/apache-install.sh")
+  vpc_security_group_ids = [aws_security_group.vpc_ssh_web.id]
+  count = 2
+  tags = {
+    Name = "My-EC2-Webserver"
+  }
+}
